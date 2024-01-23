fe427d01862e795d8648924aece626d96a1a0c97
Ron Cuijpers <r.cuijpers@hotraco.com>
2024-01-23 13:30:24 +0100

    Update repo with bug

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  items: {
+    id: string;
+    name: string;
+    checked: boolean;
+  }[] = [
+    {
+      id: '1',
+      name: 'Item 1',
+      checked: true,
+    },
+    {
+      id: '2',
+      name: 'Item 2',
+      checked: true,
+    },
+    {
+      id: '3',
+      name: 'Item 3',
+      checked: true,
+    },
+    {
+      id: '4',
+      name: 'Item 4',
+      checked: true,
+    },
+    {
+      id: '5',
+      name: 'Item 5',
+      checked: true,
+    },
+  ];
 
   constructor() {}
 
+  handleReorder(event: any) {
+    if (event.target.tagName !== 'ION-REORDER-GROUP') {
+      return;
+    }
+
+    event.detail.complete();
+    console.log('handleReorder');
+  }
+
+  handleChecked(event: any) {
+    if (event.target.tagName !== 'ION-CHECKBOX') {
+      return;
+    }
+
+    console.log('handleChecked');
+  }
 }
